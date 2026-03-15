c6bbc34dd28b2b39ea8ce6fafec82e9b5f387da3
Sachintha Dinuranga <117833055+Sachintha-Dinuranga@users.noreply.github.com>
2026-03-15 16:44:13 +0530

    feat: Add github actions ci pipeline

--- src/routes/serviceRoutes.js
+++ src/routes/serviceRoutes.js
@@ -1,29 +1,3 @@
-// import express from "express";
-// import {
-//   createService,
-//   getAllServices,
-//   getServiceById,
-//   updateService,
-//   deleteService,
-// } from "../controllers/serviceController.js";
-
-// import { createServiceValidation } from "../utils/serviceValidation.js";
-// import { validateRequest } from "../middleware/validateRequest.js";
-
-// const router = express.Router();
-
-// router.post("/", createServiceValidation, validateRequest, createService);
-
-// router.get("/", getAllServices);
-
-// router.get("/:id", getServiceById);
-
-// router.put("/:id", updateService);
-
-// router.delete("/:id", deleteService);
-
-// export default router;
-
 import express from "express";
 import {
   createService,
